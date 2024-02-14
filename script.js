8348f7011938a905498f278a29d4dae403f869bc
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-02-14 20:54:38 +0530

    feat!: Add book form boilerplate
    1. Add event listener for Add Book button

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const library = [new Book('Harry Potter And The Order Of Phoenix', 'JK Rowling', 670),];
+const addBookButton = document.getElementById('add-book');
+const bookForm = document.getElementById('book-form');
 
 function Book(title, author, numOfPages, hasRead = false) {
     this.title = title;
@@ -6,3 +8,7 @@ function Book(title, author, numOfPages, hasRead = false) {
     this.numOfPages = numOfPages;
     this.hasRead = hasRead;
 }
+
+addBookButton.addEventListener('click', () => {
+    bookForm.style.display = 'block';
+})
