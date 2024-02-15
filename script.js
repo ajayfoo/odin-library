c130c09cda03425436bffbed3d8aaedaf508064b
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-02-15 17:39:38 +0530

    feat: Add new books dynamically

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
 const library = [new Book('Harry Potter And The Order Of Phoenix', 'JK Rowling', 670),];
 const addBookButton = document.getElementById('add-book');
+const formAddBookButton = document.getElementById('form-add-book');
 const hideSidebarButton = document.getElementById('hide-sidebar');
 const sidebar = document.getElementById('sidebar');
+const bookForm = document.forms[0];
+const booksElement = document.getElementById('books');
 
 function Book(title, author, numOfPages, hasRead = false) {
     this.title = title;
     this.author = author;
-    this.numOfPages = numOfPages;
+    this.numOfPages = Number(numOfPages);
     this.hasRead = hasRead;
+}
+
+Book.prototype.getNewBookElement = function () {
+    const book = document.createElement('section');
+    book.classList.add('book');
+    const title = document.createElement('h2');
+    title.textContent = this.title;
+    const by = document.createElement('p');
+    by.textContent = 'by';
+    const author = document.createElement('p');
+    author.textContent = this.author;
+    const readStatus = document.createElement('button');
+    readStatus.textContent = this.hasRead ? 'Read' : 'Not Read';
+    book.append(title, by, author, readStatus);
+    return book;
+};
+
+function getNewBook() {
+    return new Book(
+        document.getElementById('book-title').value,
+        document.getElementById('book-author').value,
+        document.getElementById('num-of-book-pages').value,
+        document.getElementById('have-read').checked,
+    );
+}
+
+function populateBooksElement() {
+    for (const book of library) {
+        booksElement.appendChild(book.getNewBookElement());
+    }
 }
 
 addBookButton.addEventListener('click', () => {
@@ -16,4 +49,16 @@ addBookButton.addEventListener('click', () => {
 
 hideSidebarButton.addEventListener('click', () => {
     sidebar.style.display = 'none';
-})
+});
+
+formAddBookButton.addEventListener('click', (event) => {
+    if (!bookForm.checkValidity()) return;
+    event.preventDefault();
+    const newBook = getNewBook();
+    console.debug(newBook);
+    bookForm.reset();
+    library.push(newBook);
+    booksElement.appendChild(newBook.getNewBookElement());
+});
+
+populateBooksElement();
